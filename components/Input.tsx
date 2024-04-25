8fe2441e8d25f09bf8f02e1d6bc446c6e60a9f78
ritik <ritikkharya05@gmail.com>
2024-04-25 11:14:34 +0530

    updated my library and logic

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,9 +1,22 @@
-interface InputProps
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
 
-
-const Input = () => {
-  return;
-  <div>Intput!!</div>;
-};
-
+interface Inputprops extends React.InputHTMLAttributes<HTMLInputElement> {}
+const Input = forwardRef<HTMLInputElement, Inputprops>(
+  ({ className, type, disabled, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={twMerge(
+          "flex w-full p-4 my-3 bg-neutral-700 rounded-md border border-transparent px-3 py-4 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-netral-400 disabled:cursor-not-allowed disabled:opacity-50 focus:outline-none",
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Input.displayName = "Input";
 export default Input;
--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -5,10 +5,14 @@ import Input from "./Input";
 import Modal from "./Modal";
 import useuploadModal from "@/hooks/useUploadModal";
 import { useState } from "react";
-const UploadModal = () => {
-  const [isLoading, setIsLoading] = useState();
-  const UploadModal = useuploadModal();
+import Button from "./Button";
+import toast from "react-hot-toast";
+import { useUser } from "@/hooks/useUser";
 
+const UploadModal = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const UploadModal = useuploadModal();
+  const { user } = useUser();
   const { register, handleSubmit, reset } = useForm<FieldValues>({
     defaultValues: {
       author: "",
@@ -25,7 +29,22 @@ const UploadModal = () => {
     }
   };
 
-  const onSubmit: SubmitHandler<FieldValues> = async (values) => {};
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    try {
+      setIsLoading(true);
+      const imageFile = values.image?.[0];
+      const songFile = values.song?.[0];
+
+      if (!imageFile || !songFile || !user) {
+        toast.error("Missing fields");
+        return;
+      }
+    } catch (error) {
+      toast.error("something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <Modal
       title="Add a song"
@@ -38,8 +57,37 @@ const UploadModal = () => {
           id="title"
           disabled={isLoading}
           {...register("title", { required: true })}
-          placeholder="Song Title"
+          placeholder="song title"
         />
+        <Input
+          id="author"
+          disabled={isLoading}
+          {...register("author", { required: true })}
+          placeholder="song author"
+        />
+        <div>
+          Select a song file
+          <Input
+            id="song"
+            type="file"
+            disabled={isLoading}
+            accept=".mp3"
+            {...register("song", { required: true })}
+            placeholder="song"
+          />
+        </div>
+        <div>
+          Select an image
+          <Input
+            id="image"
+            type="file"
+            disabled={isLoading}
+            accept="image/*"
+            {...register("image", { required: true })}
+            placeholder="image"
+          />
+        </div>
+        <Button disabled={isLoading}>Create</Button>
       </form>
     </Modal>
   );
