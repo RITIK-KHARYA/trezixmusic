9fc3b81639c1cb7d82ee251b509081ecfb49c893
ritik <ritikkharya05@gmail.com>
2024-04-30 21:44:23 +0530

    player and function

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,7 +1,21 @@
 "use client";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
+import useGetSongById from "@/hooks/useGetSongById";
 import usePlayer from "@/hooks/usePlayer";
+
 const Player = () => {
   const player = usePlayer();
-  return <div>PLAYER</div>;
+  const { song } = useGetSongById(player.activeid);
+
+  const songUrl = useLoadSongUrl(song!);
+
+  if (!song || !songUrl || !songUrl) {
+    return null;
+  }
+  return (
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+      PLAYER
+    </div>
+  );
 };
 export default Player;
